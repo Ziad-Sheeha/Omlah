4e49874cce532ab314ca30f2fcc1ecb36a29c32d
zeyad sheeha <105352464+Ziad-Sheeha@users.noreply.github.com>
2025-06-05 20:52:47 +0300

    fix: Vercel white screen issue

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-      base: "/omlah/", // 👈 your repo name
+      base: "", // 👈 your repo name
       plugins: [vue(), vueDevTools()],
       resolve: {
             alias: {
